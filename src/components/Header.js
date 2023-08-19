6ed7f871adde747a1edb4ee98e13311fb1fd28ba
Joe <joedsonfera@hotmail.com>
2023-08-19 13:31:36 -0300

    home update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Header.css';
 import SearchIcon from '@mui/icons-material/Search';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 
 export default function Header() {
     return (
@@ -33,6 +34,12 @@ export default function Header() {
                     </span>
                     <span className="header_optionLineTwo">
                         Prime
+                    </span>
+                </div>
+                <div className="header_optionBasket">
+                    <ShoppingBasketIcon />
+                    <span className="header_optionLineTwo header_basketCount">
+                        0
                     </span>
                 </div>
             </div>
